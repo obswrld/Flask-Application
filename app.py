2d82840db050fe2cd17abccd164856747da5fc26
Republic Oba <republicoba1@gmail.com>
2025-09-11 14:23:29 +0100

    base html and database

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
+from _datetime import datetime
+
 from flask import Flask, render_template
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+Scss(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+db = SQLAlchemy(app)
+
+class MyTask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    context = db.Column(db.String(100), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"Task {self.id}"
 
 
 @app.route('/')
@@ -12,4 +27,6 @@ def index():
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
